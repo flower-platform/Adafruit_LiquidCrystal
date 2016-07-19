f81094132dabf0423e093a097446b63a4e62bf23
Andrei Taras <andrei.taras@crispico.com>
2016-07-19 18:17:33 +0300

    Testing; first pass : hello world with component wrapping the original Adafruit_LiquidCrystal library.

---
+++ LiquidCrystalComponent.h
@@ -0,0 +1,14 @@
+#ifndef LiquidCrystal_Component_h
+#define LiquidCrystal_Component_h
+
+#include "Adafruit_LiquidCrystal.h"
+
+class LiquidCrystalComponent : public Adafruit_LiquidCrystal {
+public:
+	LiquidCrystalComponent();
+	LiquidCrystalComponent(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+
+	void testExtension123();
+};
+
+#endif
