97ed9756f4649436bb1b429d93d4ed7bded99a03
Andrei Taras <andrei.taras@crispico.com>
2016-07-20 09:34:59 +0300

    Parsing attempt #1 : public properties.

--- LiquidCrystalComponent.h
+++ LiquidCrystalComponent.h
@@ -6,9 +6,6 @@
 class LiquidCrystalComponent : public Adafruit_LiquidCrystal {
 public:
 	LiquidCrystalComponent();
-	LiquidCrystalComponent(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
-
-	void testExtension123();
 };
 
 #endif
